da4e553c8dd06c9424827de35147cb50feb05ad2
joonix <jeroen@vanmenen.nl>
2022-06-21 15:14:07 +0200

    code cleanup

--- src/BildrPluginManager.ts
+++ src/BildrPluginManager.ts
@@ -6,7 +6,25 @@ export class BildrPluginManager {
     private _registeredPlugins: BildrPlugin[] = [];
 
     constructor() { }
+    
+    public register(plugin: BildrPlugin) {
+        if (plugin.name == undefined || plugin.name == null || plugin.name.trim().length == 0) {
+            throw new Error("Name is required to register a plugin.");
+        }
+        if (BildrPluginManager.isRegistered(plugin.name)) {
+            throw new Error(`Plugin with name '${plugin.name}' already registered. Name needs to be unique.`);
+        }
 
+        this.startListeningForMessagesFromIFrame();
+
+        this._registeredPlugins.push(plugin)
+        plugin.renderPage();
+    }
+
+    public isRegistered(pluginName: string): boolean {
+        return this._registeredPlugins.find(item => item.name == pluginName) != undefined
+    }
+    
     public remove(pluginName: string) {
         let plugin = this._registeredPlugins.find(item => item.name == pluginName)
         if (plugin != undefined) {
@@ -16,43 +34,33 @@ export class BildrPluginManager {
             });
         }
     }
-    public isRegistered(pluginName: string): boolean {
-        return this._registeredPlugins.find(item => item.name == pluginName) != undefined
-    }
 
     protected get window(): { addEventListener: (eventName: string, eventHandlerFunction: (e: any) => void) => void; } {
         return window;
     }
 
-    public register(plugin: BildrPlugin) {
-        if (plugin.name == undefined || plugin.name == null || plugin.name.trim().length == 0) {
-            throw new Error("Name is required to register a plugin.");
-        }
-        if (BildrPluginManager.isRegistered(plugin.name)) {
-            throw new Error(`Plugin with name '${plugin.name}' already registered. Name needs to be unique.`);
-        }
+    private startListeningForMessagesFromIFrame() {
 
-        if (this._registeredPlugins.length == 0) {
-            // Start listening for messages from iFrame/Bildr Marketplace
-            this.window.addEventListener("message", e => {
-                if (e.data) {
-                    let dataJson = JSON.parse(e.data) as BildrPluginData;
-                    if (dataJson.pluginName) {
-                        this._registeredPlugins.forEach(plugin => {
-                            if (plugin.name == dataJson.pluginName) {
-                                if (dataJson.data == undefined) dataJson.data = {};
-                                dataJson.data.msgId = dataJson.msgId;
-                                plugin.triggerAction(dataJson.command, dataJson.data)
-                            }
-                        })
-                    }
-                }
-            });
-        }
+        if (this._registeredPlugins.length != 0) return;
 
-        this._registeredPlugins.push(plugin)
-        plugin.renderPage();
+        this.window.addEventListener("message", e => {
+            this.triggerActionInPlugin(e);
+        });
+    }
 
+    private triggerActionInPlugin(e: any) {
+        if (!e.data) return;
+
+        let dataJson = JSON.parse(e.data) as BildrPluginData;
+
+        let plugin = this._registeredPlugins.find(item => dataJson.pluginName && item.name == dataJson.pluginName);
+
+        if (plugin == undefined) return;
+
+        if (dataJson.data == undefined) dataJson.data = {};
+        dataJson.data.msgId = dataJson.msgId;
+
+        plugin.triggerAction(dataJson.command, dataJson.data);
     }
 
     // STATIC FUNCTIONS
@@ -73,6 +81,6 @@ type BildrPluginData = {
     pluginName: string,
     command: string,
     msgId: string,
-    data : any
+    data: any
 }
 
